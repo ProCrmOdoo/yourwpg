ad58c1ceb1ebf2bceadbb18923fa317a05db5d73
anastasiia <evdokimova.arh@gmail.com>
2024-04-03 16:32:29 +0300

    test

---
+++ custom_field/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Procrm: custom field",
+    "version": "14.0.1.0.0",
+    "author": "Yevdokimova A.Y.",
+    "company": "Procrm",
+    "depends": ["base", "web", "crm", "sale"],
+    "data": [
+        "views/crm_lead_view.xml",
+        "views/res_partner_view.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
